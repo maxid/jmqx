412654440a17a70fa881d2b169cd51694c1d6bae
maxid <maxid@qq.com>
2025-09-23 16:53:55 +0800

    modify: 增加注释

--- jmqx-broker/src/main/java/plus/jmqx/broker/mqtt/channel/traffic/TrafficHandlerLoader.java
+++ jmqx-broker/src/main/java/plus/jmqx/broker/mqtt/channel/traffic/TrafficHandlerLoader.java
@@ -3,7 +3,7 @@ package plus.jmqx.broker.mqtt.channel.traffic;
 import io.netty.handler.traffic.AbstractTrafficShapingHandler;
 
 /**
- * 尽量简洁一句描述
+ * 流控
  *
  * @author maxid
  * @since 2025/4/10 17:25
