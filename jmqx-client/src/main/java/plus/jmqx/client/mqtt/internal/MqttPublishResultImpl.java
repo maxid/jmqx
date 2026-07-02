41a1feda90781be96e2a2079b79014bd442c6a10
maxid <maxid@qq.com>
2026-07-02 18:54:01 +0800

    feat(client): add AckTracker for outbound QoS1/2 ACK tracking (TDD)

---
+++ jmqx-client/src/main/java/plus/jmqx/client/mqtt/internal/MqttPublishResultImpl.java
@@ -0,0 +1,22 @@
+package plus.jmqx.client.mqtt.internal;
+
+import plus.jmqx.client.mqtt.message.MqttPublish;
+import plus.jmqx.client.mqtt.message.MqttPublishResult;
+
+/**
+ * 版本无关的 {@link MqttPublishResult} 实现。
+ *
+ * @author maxid
+ */
+public record MqttPublishResultImpl(MqttPublish publish, Throwable error) implements MqttPublishResult {
+
+    @Override
+    public MqttPublish getPublish() {
+        return publish;
+    }
+
+    @Override
+    public Throwable getError() {
+        return error;
+    }
+}
---
+++ jmqx-client/src/main/java/plus/jmqx/client/mqtt/internal/PendingOutbound.java
@@ -0,0 +1,35 @@
+package plus.jmqx.client.mqtt.internal;
+
+import plus.jmqx.client.mqtt.message.MqttPublish;
+import plus.jmqx.client.mqtt.message.MqttPublishResult;
+import reactor.core.publisher.Sinks;
+
+/**
+ * 一个等待 ACK 的出站 QoS1/2 消息。
+ *
+ * @author maxid
+ */
+public final class PendingOutbound {
+
+    private final MqttPublish publish;
+    private final Sinks.One<MqttPublishResult> resultSink;
+    private final long sentNanos;
+
+    public PendingOutbound(MqttPublish publish, Sinks.One<MqttPublishResult> resultSink) {
+        this.publish = publish;
+        this.resultSink = resultSink;
+        this.sentNanos = System.nanoTime();
+    }
+
+    public MqttPublish getPublish() {
+        return publish;
+    }
+
+    public Sinks.One<MqttPublishResult> getResultSink() {
+        return resultSink;
+    }
+
+    public long getSentNanos() {
+        return sentNanos;
+    }
+}
